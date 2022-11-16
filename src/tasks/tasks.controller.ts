278dd24ad1489f113d581f0520bb6e4992832b76
Karoline Alencar <karolinedalencar@gmail.com>
2022-11-16 19:54:16 -0300

    chore: fix import path

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -10,8 +10,8 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { User } from 'src/auth/user.entity';
-import { GetUser } from 'src/get-user.decorator';
+import { User } from '../auth/user.entity';
+import { GetUser } from '../get-user.decorator';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
